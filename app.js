a611e4cc5f74da8a36cb23cb00f978e0197ed5fd
analogpixel <matt.poepping@gmail.com>
2021-01-04 13:36:30 -0700

    more functions

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // https://github.com/tsironis/lockr
 
 var user_data;
-var user_contacts = [{'name':'matt', 'last':'12/12/2020'}, {'name':'jane', 'last':'12/12/2020'}];
+var user_contacts = [];
 var current_selected;
 Lockr.prefix = 'lockr_';
 const qrScanner = new QrScanner(document.getElementById('vid'), result => process_message(result));
@@ -22,8 +22,8 @@ function load_data() {
 }
 
 function save_data() {
-  Locker.set('user_data', user_data);
-  Locker.set('user_contacts', user_contacts);
+  Lockr.set('user_data', user_data);
+  Lockr.set('user_contacts', user_contacts);
 }
 
 function update_ui() {
@@ -53,8 +53,39 @@ function update_message_pane() {
 add a user to the contact list
 */
 function add_user_ui(u) {
-  a = `<div id="${name_to_id(u.name)}" onClick="update_person(this)" class="person"><div class="person-name">${u.name}</div><div class="person-date">${u.last}</div></div>`
+  a = `<div id="${name_to_id(u.first_name)}" onClick="update_person(this)" class="person"><div class="person-name">${u.first_name}</div><div class="person-date">${u.last}</div></div>`
   $("#contact_list").append(a);
 }
 
+function print_message() {
+  new QRCode(document.getElementById("qrcode"), {
+      text: "m:this is the text",
+      width: 800,
+      height: 800,
+      correctLevel : QRCode.CorrectLevel.L
+  });
+
+  window.print();
+}
+
+// Save the user data to local storage
+document.getElementById("save_user_data").addEventListener("click", function() {
+
+  user = {'first_name': $("#first_name_input").val(),
+          'last_name':  $("#last_name_input").val(),
+          'user_address': $("#user_address").val(),
+          'user_city': $("#user_city").val(),
+          'user_state': $("#user_state").val(),
+          'user_zip': $("#user_zip").val()
+          };
+
+  console.log("write user:", user);
+
+  user_contacts.push(user);
+  save_data(); // save the data to disk
+  update_ui(); // update the ui
+  $("#newUser").modal('toggle');
+}); 
+
+load_data();
 update_ui();
